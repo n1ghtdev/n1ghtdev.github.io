7e2f6615807de39b59cfb15e5f0fc83a566f5c8c
Nickew <Extr1m7777@gmail.com>
2019-06-16 21:33:01 +0300

    add Socials links

--- src/containers/Socials.jsx
+++ src/containers/Socials.jsx
@@ -12,9 +12,30 @@ const StyledNav = styled(Nav)`
 
 const SocialsList = () => (
   <StyledNav Color="#fff" FontFamily="'Ubuntu Mono', monospace">
-    <NavItem href="#telegram">telegram</NavItem>
-    <NavItem href="#facebook">facebook</NavItem>
-    <NavItem href="#github">github</NavItem>
+    <NavItem
+      href="https://t.me/n1ckew"
+      title="Message me in Telegram"
+      target="_blank"
+      rel="noopener"
+    >
+      telegram
+    </NavItem>
+    <NavItem
+      href="https://www.facebook.com/profile.php?id=100001510733487"
+      title="My facebook page"
+      target="_blank"
+      rel="noopener"
+    >
+      facebook
+    </NavItem>
+    <NavItem
+      href="https://github.com/Nickew"
+      title="My github page"
+      target="_blank"
+      rel="noopener"
+    >
+      github
+    </NavItem>
   </StyledNav>
 );
 
