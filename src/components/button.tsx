0e791a08ab37e592be5197bca2b9de8db7579651
n1ghtdev <extr1m7777@gmail.com>
2020-08-08 18:03:41 +0300

    refactor: button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,57 +1,43 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+import ButtonBase from './button-base';
 
 type Props = {
   href?: string;
-  type?: "button" | "submit";
+  to?: string;
+  type?: 'button' | 'submit';
   onClick?: (evt: React.MouseEvent<HTMLElement>) => void;
   children: any;
 };
 
-const StyledButton = styled.a`
-  display: inline-block;
-  position: relative;
-  font-size: 22px;
-  transition: all 250ms;
-
-  &:before {
-    content: "";
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    height: 100%;
-    transition: all 150ms ease-out;
-    background: ${({ theme }) => theme.secondary};
-    opacity: 0.5;
-  }
-
-  &:hover:before {
-    height: 6px;
-    transform: translateY(22px);
-    background: ${({ theme }) => theme.primary};
-    opacity: 1;
-  }
-`;
-
 function Button(props: Props) {
   const { href, onClick, type, children, ...rest } = props;
+
   if (props.href) {
     return (
-      <StyledButton
+      <ButtonBase
+        as="a"
         href={href}
         target="_blank"
         rel="nofollow noopener noreferrer"
         {...rest}
       >
         {children}
-      </StyledButton>
+      </ButtonBase>
+    );
+  } else if (props.to) {
+    return (
+      <ButtonBase as={Link} to={props.to} {...rest}>
+        {children}
+      </ButtonBase>
     );
   }
   return (
-    <StyledButton as="button" onClick={onClick} type={type || "button"}>
+    <ButtonBase as="button" onClick={onClick} type={type || 'button'}>
       {children}
-    </StyledButton>
+    </ButtonBase>
   );
 }
 
